0f48c7bae2f7cd694c4a66ae3c8883ff86a5e630
cpicha20 <cpichardowps@gmail.com>
2023-11-03 23:45:48 -0400

    readme + tested

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # E-Commerce-Back-End
+
+UCF Coding Bootcamp Week 7 Challange 1. 
+
+
+The purpose of this challenge was to learn how to familiarize, implement , and utilize object-relational mapping, or ORM. Using the given code, I created Models for Categoreis, Products, Tags, and Product Tags. I also established routes for all of these so that they can be viewed, created, updated, and deleted. 
+
+
+
+## Tech Stack
+
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![Sequelize](https://img.shields.io/badge/Sequelize-52B0E7?style=for-the-badge&logo=Sequelize&logoColor=white)
+![MySQL](https://img.shields.io/badge/mysql-%2300f.svg?style=for-the-badge&logo=mysql&logoColor=white)
+![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB)
+
--- models/index.js
+++ models/index.js
@@ -1,38 +1,32 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id',
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+  foreignKey: "category_id",
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   // Define the third table needed to store the foreign keys
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  // Define an alias for when data is retrieved
-  as: 'product_tags'
+  through: ProductTag,
+  foreignKey: "product_id",
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   // Define the third table needed to store the foreign keys
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  // Define an alias for when data is retrieved
-  as: 'product_tags'
+  through: ProductTag,
+  foreignKey: 'tag_id',
 });
+
+
 module.exports = {
   Product,
   Category,
